5859026aec3458c3a279c9d7a455365016e0ceec
Toluwalashe <asheogunleye02@gmail.com>
2023-05-15 19:43:01 +0000

    mistake fix

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import orderSlice from './orderSlice'
 import productSlice from './productSlice'
-import reviewSlice from './reviewSlice'
 import userSlice from './userSlice'
 import uiSlice from './uiSlice'
 
@@ -9,7 +8,6 @@ const store = configureStore({
     reducer: {
         order: orderSlice.reducer,
         product: productSlice.reducer,
-        review: reviewSlice.reducer,
         user: userSlice.reducer,
         ui: uiSlice.reducer
     }
